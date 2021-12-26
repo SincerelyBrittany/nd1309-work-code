bf44b5771681925addf5c0c8e02343c6545edb0c
Sincerely Brittany <sincerelybr@gmail.com>
2021-12-25 22:18:52 -0500

    adds blockchain class

--- Project_2/simpleChain.js
+++ Project_2/simpleChain.js
@@ -12,3 +12,26 @@ class Block {
       this.hash = '';
     }
   }
+
+  /* ===== Blockchain ===================================
+|  Class with a constructor for blockchain data model  |
+|  with functions to support:                          |
+|     - createGenesisBlock()                           |
+|     - getLatestBlock()                               |
+|     - addBlock()                                     |
+|     - getBlock()                                     |
+|     - validateBlock()                                |
+|     - validateChain()                                |
+|  ====================================================*/
+
+class Blockchain{
+  constructor(){
+    // new chain array
+    this.chain = [];
+}
+
+// addBlock method
+addBlock(newBlock){
+   this.chain.push(newBlock);
+}
+}
